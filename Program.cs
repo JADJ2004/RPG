afefed9fcfdaebabd446db180deeefd6829d49db
Jibbe <jibbedejong@hotmail.com>
2024-02-18 20:17:54 +0100

    quit game mechanic

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-public class Program { static void Main(string[] args) { } }
+using System;
+
+namespace Placeholder
+{
+    public class Program
+    {
+        private static bool gameRunning = true;
+
+        public static void Main(string[] args)
+        {   
+            while (gameRunning)
+            {
+                if (Console.KeyAvailable)
+                {
+                    string userInput = Console.ReadLine();
+
+                    if (string.Equals(userInput, "quit", StringComparison.OrdinalIgnoreCase))
+                    {
+                        gameRunning = false;
+                        continue
+                    }
+                }
+                // zet hier de rest van de code neer
+            }
+        }
+    }
+}
