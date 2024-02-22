a23845f7644d74e6d83ae9dfd3c65d0a3033f9a1
ELENAJACOBS_2017@YAHOO.COM <146252282+ECarolinaJacobs@users.noreply.github.com>
2024-02-22 22:20:34 +0100

    Update Weapon.cs
    inventory basic

--- Weapon.cs
+++ Weapon.cs
@@ -1,1 +1,26 @@
-public class Weapon {}
+public class Weapon
+{
+    public string Name {get; set;}
+    public Weapon(string name)
+    {
+        Name = name;
+    }
+    public List<Weapon> Weapons = new List<Weapon>();
+
+    public void addWeapon(Weapon item)
+    {
+        Weapons.Add(item);
+    }
+
+    public void removeWeapon(Weapon item)
+    {
+        Weapons.Remove(item);
+    }
+    public void printWeapons()
+    {
+        foreach (var weapon in Weapons)
+        {
+            Console.WriteLine(weapon.Name);
+        }
+    }
+}
