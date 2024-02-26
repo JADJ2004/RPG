b254f874e0735220617afe998777a9120b716bbb
CS0nder <152660399+CS0nder@users.noreply.github.com>
2024-02-26 15:25:03 +0100

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,5 +1,6 @@
 public class Player 
 {   public int TotalGold = 0;
+    public int Health = 100;
     public void AddGold(int gold)
     {
         TotalGold += gold;
@@ -7,6 +8,21 @@ public class Player
     public void RemoveGold(int gold)
     {
         TotalGold -= gold;
+    }
+
+    public void IncreaseHealth(int health)
+    {
+        if (Health != 100)
+        {
+            Health += health;
+        }
+    }
+    public void DecreaseHealth(int health)
+    {
+        if (Health != 0)
+        {
+            Health -= health;
+        }
     }
 
 }
