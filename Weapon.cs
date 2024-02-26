f80fe40c7c1ea0712aa3b70e57dfcbf1b4c8a36a
JADJ2004 <95543180+JADJ2004@users.noreply.github.com>
2024-02-26 13:19:31 +0100

    Item 13

--- Weapon.cs
+++ Weapon.cs
@@ -1,26 +1,33 @@
 public class Weapon
 {
-    public string Name {get; set;}
-    public Weapon(string name)
+    public int ID { get; private set; }
+    public string Name { get; private set; }
+    public int Damage { get; private set; }
+
+    public Weapon(int id, string name, int damage)
     {
+        ID = id;
         Name = name;
+        Damage = damage;
     }
+
     public List<Weapon> Weapons = new List<Weapon>();
 
-    public void addWeapon(Weapon item)
+    public void AddWeapon(Weapon item)
     {
         Weapons.Add(item);
     }
 
-    public void removeWeapon(Weapon item)
+    public void RemoveWeapon(Weapon item)
     {
         Weapons.Remove(item);
     }
-    public void printWeapons()
+
+    public void PrintWeapons()
     {
         foreach (var weapon in Weapons)
         {
-            Console.WriteLine(weapon.Name);
+            Console.WriteLine($"{weapon.Name}, Damage: {weapon.Damage}");
         }
     }
 }
